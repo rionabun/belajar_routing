5882d356995dc74406de7bcde68c9443e5662503
rionabun <rion_novel@yahoo.com>
2019-08-27 14:26:52 +0700

    Add files via upload

---
+++ build/precache-manifest.fa73ca0699bb45003dc273cb7b7d71ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e28a4d244390adf336a3e236edb5980",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61ff6b18655b62d5a71c",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d23b96ad1b08e001a902",
+    "url": "/static/js/2.96753879.chunk.js"
+  },
+  {
+    "revision": "61ff6b18655b62d5a71c",
+    "url": "/static/js/main.51976a97.chunk.js"
+  },
+  {
+    "revision": "d3b9c27f6ed89f669cbc",
+    "url": "/static/js/runtime~main.06854618.js"
+  }
+]);
